294985775da3d21c9ad4138f384020278d6a80be
jassyio <joejassyio@gmail.com>
2025-05-19 19:50:54 +0300

    backend url change

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3001;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://your-frontend.vercel.app",
+  "ultra-backend.vercel.app ",
 ];
 
 app.use(cors({
