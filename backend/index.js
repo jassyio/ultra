98180d4f49b5d0361e055ca18103af1eb346b70e
jassyio <joejassyio@gmail.com>
2025-06-06 11:41:18 +0300

    removed terailing / on index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3001;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3001",
-  "https://ultra-frontend-zeta.vercel.app/",
+  "https://ultra-frontend-zeta.vercel.app",
 ];
 
 app.use(cors({
